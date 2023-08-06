bc33002734e7b05933da3df4624ce303e93eff9b
aditya upadhyaya <mraditya345@gmail.com>
2023-08-06 17:35:44 +0530

    Adding CRUD operation for Student Management

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { Student } from 'src/app/model/student';
 import { AuthService } from 'src/app/shared/auth.service';
+import { DataService } from 'src/app/shared/data.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,9 +10,85 @@ import { AuthService } from 'src/app/shared/auth.service';
 })
 export class DashboardComponent {
 
-  constructor(private auth: AuthService) {}
+  studentsList : Student[] = [];
+  studentObj : Student = {
+    id: '',
+    first_name: '',
+    last_name: '',
+    email: '',
+    mobile: ''
+  };
+  id: string = '';
+  first_name: string = '';
+  last_name: string = '';
+  email: string = '';
+  mobile: string = '';
+
+  constructor(private auth: AuthService, private data : DataService) {}
+
+  ngOninit() {
+    this.getAllStudents();
+  }
 
   logout() {
     this.auth.logout();
   }
+
+  getAllStudents() {
+    this.data.getAllStudents().subscribe(res => {
+      this.studentsList = res.map((e: any) => {
+        const data = e.payload.doc.data;
+        data.id = e.payload.doc.id;
+        return data;
+      })
+    }, err => {
+      alert('Error while fetching students list');
+    });
+  }
+
+  resetForm() {
+    this.id = '';
+    this.first_name = '';
+    this.last_name = '';
+    this.email = '';
+    this.mobile = '';
+  }
+
+  addStudent() {
+    if(this.first_name == '' || this.last_name =='' || this.email == '' || this.mobile == '') {
+      alert('Fill all the input fields');
+      return;
+    }
+
+    this.studentObj.id = '';
+    this.studentObj.first_name = this.first_name;
+    this.studentObj.last_name = this.last_name;
+    this.studentObj.email = this.email;
+    this.studentObj.mobile = this.mobile;
+
+    this.data.addStudent(this.studentObj);
+    this.resetForm();
+  }
+
+  updateStudent() {
+
+  }
+
+  deleteStudent(student: Student) {
+    if(window.confirm('Are you sure you want to delete - ' + student.first_name + ' ' + student.last_name + ' ' +'?')) {
+      this.data.deleteStudent(student);
+
+  } 
+    }
+      
+
+
+
+
+
+
+
+
+
+
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,11 @@
 export const environment = {
 
-    //Enter from Firebase
+  firebase: {
+    projectId: 'student-management-caf25',
+    appId: '1:439820640848:web:172b11983af3a55a0de755',
+    storageBucket: 'student-management-caf25.appspot.com',
+    apiKey: 'AIzaSyCDr0Vp7NNXyP9erH9QsGuhYGAPSu9Al3k',
+    authDomain: 'student-management-caf25.firebaseapp.com',
+    messagingSenderId: '439820640848',
+  },
 };
